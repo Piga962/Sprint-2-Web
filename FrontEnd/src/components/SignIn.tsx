d1d2f037bb936664ef985a86d1a51df82e71a8dd
rafarm04 <wafar0mo@hotmail.com>
2025-05-01 12:03:13 -0600

    Added Login component

---
+++ FrontEnd/src/components/SignIn.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function SignIn() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <input type="email" />
+      <input type="password" />
+    </div>
+  );
+}
