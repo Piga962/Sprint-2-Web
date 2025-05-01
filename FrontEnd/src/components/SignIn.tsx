c39bfb55a1529e70b8a2fe3891232485533f4a45
rafarm04 <wafar0mo@hotmail.com>
2025-05-01 12:21:25 -0600

    x

--- FrontEnd/src/components/SignIn.tsx
+++ FrontEnd/src/components/SignIn.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Login</h1>
       <input type="email" />
       <input type="password" />
+      <button onClick={}>Login</button>
     </div>
   );
 }
